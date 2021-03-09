622f56064d5b69686fbceb8d80ab38270e43e696
Abdinajib Liban <abdinajibliban@gmail.com>
2021-03-09 16:37:23 -0500

    setup title component

--- employeedirectory/src/components/title.js
+++ employeedirectory/src/components/title.js
@@ -1,1 +1,13 @@
-//initial title
+
+import React from "react";
+
+function title() {
+    return (
+        <div className="text-center bg-info p-5">
+            <h1 style={{ fontSize: 50, color: "white" }}>Staff Roster</h1>
+            <p style={{ color: "white" }}>Employee Record</p>
+        </div>
+    )
+}
+
+export default title;
